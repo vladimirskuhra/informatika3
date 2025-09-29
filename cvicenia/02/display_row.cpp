579c37e0561f2db8f29abf042fd932138b197ef6
vladimirskuhra <skuhra4@stud.uniza.sk>
2025-09-29 11:34:44 +0200

    commit -m "koniec cvicenia 02"

--- cvicenia/02/display_row.cpp
+++ cvicenia/02/display_row.cpp
@@ -2,7 +2,12 @@
 
 using namespace asciid;
 
-DisplayRow::DisplayRow(/*args*/)
+DisplayRow::DisplayRow()
+    : m_pixels(8) // inicializacia atributov, tato funkcia m_pixels bude mat hodnotu 8
 {
 
+}
+
+DisplayRow::~DisplayRow() 
+{
 }
